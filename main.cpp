c79ac87a5e7cbbb8b29980cd1afd153a275fbee4
Slapjacky <jerikolo1@gmail.com>
2020-08-03 00:49:31 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+// defines uint as unsigned int for an easier time
+using uint = unsigned int;
+
+/*
+basically a street life game, get rich, sell drugs, that kind of thing.
+player needs to earn respect for certain things, maybe get different missions when they get certain respect
+money for buying stuff, trading, meds -> inventory system
+
+IDEA: drugs that change every text into another format that makes shit look whacky af: lIkE ThIs MaYbE bUUT smOnethinG WiEIRd
+*/
+int main()
+{
+
+    mainMenu();
+
+    std::cout << "Hello World!" std::endl;
+
+    return 0;
+}
